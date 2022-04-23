75ef52c0c6b5a8775fc8d9ca8e3a7f4aba93b82d
91818 <91818@LAPTOP-A1HD18TF>
2022-04-23 13:51:32 +0530

    first

---
+++ Java_DataAbstraction program/src/java_abstraction/DataAbstraction.java
@@ -0,0 +1,23 @@
+package java_abstraction;
+abstract class atm
+{
+	abstract void withdraw();
+}
+
+public class DataAbstraction extends atm
+{
+	void withdraw()
+	{
+		System.out.println("withdraw successfully");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DataAbstraction obj=new DataAbstraction();
+		obj.withdraw();
+		
+
+	}
+
+}
