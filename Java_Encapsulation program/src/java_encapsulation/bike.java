10dd7b4e777ce11c89f7d05cd01215720625863d
91818 <91818@LAPTOP-A1HD18TF>
2022-04-23 13:47:11 +0530

    first

---
+++ Java_Encapsulation program/src/java_encapsulation/bike.java
@@ -0,0 +1,29 @@
+package java_encapsulation;
+
+public class bike
+{
+	int year_ofpurchase;
+	String make;
+	int model;
+	double cost;
+	public void display1()  //member function 
+	{
+		System.out.println("year of purchse" +2001);
+		System.out.println("make"+ "suzuki");
+	}
+	public void display2()
+	{
+		System.out.println("model"+1999);
+		System.out.println("cost"+20000);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bike obj=new bike();
+		obj.display1();
+		obj.display2();
+		
+
+	}
+
+}
