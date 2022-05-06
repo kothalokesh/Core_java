536ab5c2fd17d8eb336bebffe61b51fbf4e8d063
91818 <91818@LAPTOP-A1HD18TF>
2022-05-06 13:44:56 +0530

    array program

---
+++ arrays/src/arrays/arr.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class arr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a= new int [3];
+		a[0]=99;
+		a[1]=55;
+		a[2]=44;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		
+
+	}
+
+}
