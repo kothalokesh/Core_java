d6cb9a95181a6f8bcf2f5734e8f4afc3580f6624
91818 <91818@LAPTOP-A1HD18TF>
2022-05-06 13:48:56 +0530

    ARRAY LIST

---
+++ arraylist/src/arraylist/arrl.java
@@ -0,0 +1,19 @@
+package arraylist;
+import java.util.ArrayList;
+
+public class arrl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	ArrayList<String> a1 =new ArrayList<>();
+			
+		a1.add("hi");
+		a1.add("hello");
+		a1.add("ram");
+		a1.add("new");
+		System.out.println(a1);
+		
+
+	}
+
+}
