910ad648f6a2a79e6106b74eea2403fbc046509d
91818 <91818@LAPTOP-A1HD18TF>
2022-05-06 13:50:44 +0530

    TRY CATCH  IN ARRAYS

---
+++ try_catch_arrays/src/try_catch_arrays/withoutexe.java
@@ -0,0 +1,19 @@
+package try_catch_arrays;
+
+public class withoutexe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+		int a[]= {1,2,3,4,5};
+		System.out.println(a[10]);
+		}
+		catch(Exception e)
+		{
+			System.out.println("element does not valid");
+		}
+
+	}
+
+}
