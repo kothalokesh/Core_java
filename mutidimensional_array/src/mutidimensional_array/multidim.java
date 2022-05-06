c53a6268694e80f9184c12bf6486c703c6a29c29
91818 <91818@LAPTOP-A1HD18TF>
2022-05-06 13:46:52 +0530

    multidimensional array

---
+++ mutidimensional_array/src/mutidimensional_array/multidim.java
@@ -0,0 +1,18 @@
+package mutidimensional_array;
+
+public class multidim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]= {{2,7,9},{3,6,1},{7,4,1}};
+		for(int i=0;i<3;i++)
+		{
+			for (int j=0;j<3;j++)
+				System.out.println( arr[i][j]);
+			
+				System.out.println();
+		}
+
+	}
+
+}
---
+++ mutidimensional_array/src/mutidimensional_array/package-info.java
@@ -0,0 +1,1 @@
+package mutidimensional_array;
