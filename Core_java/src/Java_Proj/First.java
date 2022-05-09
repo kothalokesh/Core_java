0d7e4d522b405ab609fd486b8b5c6ff25f2e0df4
91818 <91818@LAPTOP-A1HD18TF>
2022-05-09 14:05:13 +0530

    first

--- Core_java/src/Java_Proj/First.java
+++ Core_java/src/Java_Proj/First.java
@@ -3,11 +3,8 @@ package Java_Proj;//pakage name
 public class First {//class name
 
 	public static void main(String[] args) {
-		System.out.println("hello java");/*printer
-		8eutie09ut8ju8
-		etijtdipjvoirjvwoio
-		trjoiejoijeoip
-		*/
+		System.out.println("hello java");
+		
 	
 
 	}
