3e421e924c989c9f41c662c8424adf4838ec2fb6
91818 <91818@LAPTOP-A1HD18TF>
2022-05-16 19:12:38 +0530

    JAVA COLLECTION IN STACK Program

---
+++ collection_java/src/collection_java/stackpg.java
@@ -0,0 +1,27 @@
+package collection_java;
+import java.util.*;
+
+public class stackpg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> obj=new Stack();
+		
+		obj.push(30);
+		obj.push(80);
+		obj.push(90);
+		System.out.println(obj);
+		obj.pop();
+		obj.pop();
+		System.out.print(obj);
+	
+ System.out.println(obj.peek());
+ 
+//obj.remove(0);
+ //System.out.println(obj);
+ 
+
+
+	}
+
+}
