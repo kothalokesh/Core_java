e7ba1a9d372cda3317dab95ebc80313522428f65
91818 <91818@LAPTOP-A1HD18TF>
2022-05-16 15:45:02 +0530

    SYNCHRONIZED keyword program

---
+++ synchronised_keywordpg/src/synchronised_keywordpg/package-info.java
@@ -0,0 +1,1 @@
+package synchronised_keywordpg;
---
+++ synchronised_keywordpg/src/synchronised_keywordpg/syncdemo.java
@@ -0,0 +1,52 @@
+package synchronised_keywordpg;
+class counter
+{
+	int count;
+	public synchronized void inc()
+	{
+	 count++;
+	}
+}
+
+public class syncdemo {
+
+	public static void main(String[] args) throws Exception 
+	{
+		// TODO Auto-generated method stub
+		counter ob=new counter();
+		
+		
+		
+		Thread t1=new Thread(new Runnable()
+          {
+			
+			public void run()
+			{
+				for(int i=0;i<1000;i++)
+				{
+			 ob.inc();
+			}
+				System.out.println("count is"+ob.count);
+			}
+				});
+		
+		
+		
+	Thread t2=new Thread(new Runnable()
+				{
+			public void run()
+			{
+				for(int i=0;i<2000;i++)
+				{
+					ob.inc();
+				}
+				System.out.println("count is"+ob.count);
+			}
+				});
+t1.start();
+t2.start();
+//t1.join();
+//t2.join();
+	}
+
+}
