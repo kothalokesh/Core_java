bcf4cfa9ee8f2cbe6c39988d85b141411edf181c
91818 <91818@LAPTOP-A1HD18TF>
2022-05-23 14:38:08 +0530

    VECTOR program

---
+++ VECTORPROGRAM/src/VECTORPG/GFG.java
@@ -0,0 +1,20 @@
+package VECTORPG;
+import java.util.*;
+public class GFG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Vector<Integer> v =new Vector<Integer>();
+ for(int i=1;i<=5;i++)
+	 v.add(i);
+	 System.out.println(v);
+	 v.remove(3);
+	 System.out.println(v);
+	 for(int i=0;i<=v.size();i++)
+	 
+		 System.out.println(v.get(i)+"");
+ 
+ 
+	}
+
+}
