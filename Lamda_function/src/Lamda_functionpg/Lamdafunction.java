fa28d9a08f12166fe3b3235d121ac5b54592677a
91818 <91818@LAPTOP-A1HD18TF>
2022-06-01 21:27:32 +0530

    LAMDA FUNCTION program

--- Lamda_function/src/Lamda_functionpg/Lamdafunction.java
+++ Lamda_function/src/Lamda_functionpg/Lamdafunction.java
@@ -11,8 +11,8 @@ public class Lamdafunction {
 		// TODO Auto-generated method stub
 		cube c=(int a,int b) ->a+b;
 		int res=c.cal(5,10);
-		System.out.println(res);
-		System.out.println(res);
+	System.out.println(res);
+		
 
 	}
 
