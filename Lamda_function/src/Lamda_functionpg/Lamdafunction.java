ea7404ce9ace01fc593ef8c8868465308cc34a67
91818 <91818@LAPTOP-A1HD18TF>
2022-06-01 21:04:08 +0530

    LAMDA FUNCTION program

---
+++ Lamda_function/src/Lamda_functionpg/Lamdafunction.java
@@ -0,0 +1,18 @@
+package Lamda_functionpg;
+interface cube
+{
+	int cal(int a,int b);
+	
+}
+
+public class Lamdafunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		cube c=(int a,int b) ->a+b;
+		int res=c.cal(5,10);
+		System.out.println(res);
+
+	}
+
+}
