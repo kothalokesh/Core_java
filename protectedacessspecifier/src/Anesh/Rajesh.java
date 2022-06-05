9294a7112ae5fd935568a050895baeaf783b496e
91818 <91818@LAPTOP-A1HD18TF>
2022-06-05 12:22:39 +0530

    PROTECTED acessspecifier pkg

---
+++ protectedacessspecifier/src/Anesh/Rajesh.java
@@ -0,0 +1,12 @@
+package Anesh;
+import protectedacessspecifier.*;
+public class Rajesh extends Anesh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rajesh obj =new Rajesh();
+		System.out.println(obj.addTwoNumbers(11,22));
+
+	}
+
+}
---
+++ protectedacessspecifier/src/protectedacessspecifier/Anesh.java
@@ -0,0 +1,11 @@
+package protectedacessspecifier;
+
+public class Anesh {
+		protected int addTwoNumbers(int a,int b)
+{
+	return a+b;
+
+
+	}
+
+}
