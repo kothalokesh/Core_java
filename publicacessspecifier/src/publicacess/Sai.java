6df760cacba25e4cff735d3a736333ad11ee6220
91818 <91818@LAPTOP-A1HD18TF>
2022-06-05 12:29:26 +0530

    PUBLIC acessspecifiers pkg

---
+++ publicacessspecifier/src/publicacess/Sai.java
@@ -0,0 +1,13 @@
+package publicacess;
+import publicacessspecifier.*;
+
+public class Sai extends Manoj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Manoj obj=new Manoj();
+		System.out.println(obj.addTwoNumbers(50, 50));
+
+	}
+
+}
---
+++ publicacessspecifier/src/publicacessspecifier/Manoj.java
@@ -0,0 +1,9 @@
+package publicacessspecifier;
+
+public class Manoj {
+	public int addTwoNumbers(int a,int b)
+	{
+		return a+b;
+	}
+
+}
