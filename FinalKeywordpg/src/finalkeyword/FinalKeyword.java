78c029a2ca9961d14b81a6d39c19a1e03f977066
91818 <91818@LAPTOP-A1HD18TF>
2022-06-25 17:05:41 +0530

    FINAL KEYWORD pg

---
+++ FinalKeywordpg/src/finalkeyword/FinalKeyword.java
@@ -0,0 +1,18 @@
+package finalkeyword;
+
+public class FinalKeyword {
+final int a=100;
+final void run()
+{
+	int a=100;
+	System.out.println("final Metod");
+}
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinalKeyword ob = new FinalKeyword();
+		ob.run();
+
+	}
+
+}
