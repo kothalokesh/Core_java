48a885003ee596240d22bda56751f35fbd67d174
91818 <91818@LAPTOP-A1HD18TF>
2022-06-25 16:54:22 +0530

    STATIC NESTEDCLASS pg

---
+++ StaticNestedClass/src/staticnestedclass/NestedClass.java
@@ -0,0 +1,21 @@
+package staticnestedclass;
+
+public class NestedClass {
+	static class a
+	{
+		static void disp()
+		{
+			int a=100;
+			System.out.println(a);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+a.disp();
+
+
+	}
+
+}
