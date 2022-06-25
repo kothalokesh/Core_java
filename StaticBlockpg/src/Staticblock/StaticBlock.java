251e56eb167076e4020cf6ced281b97c5ea910aa
91818 <91818@LAPTOP-A1HD18TF>
2022-06-25 16:42:15 +0530

    STATIC_BLOCK pg

---
+++ StaticBlockpg/src/Staticblock/StaticBlock.java
@@ -0,0 +1,21 @@
+package Staticblock;
+
+public class StaticBlock {
+	static int a=100;
+
+static
+{
+	a=200;
+	System.out.println(a);
+}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticBlock a=new StaticBlock();
+		
+		
+
+	}
+
+}
