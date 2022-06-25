0c6ca941567a29e12fcc43fb764423d5ee01dcc3
91818 <91818@LAPTOP-A1HD18TF>
2022-06-25 16:36:00 +0530

    STATIC METHOD pg

---
+++ StaticMethod/src/staticmethod/StaticMethod.java
@@ -0,0 +1,15 @@
+package staticmethod;
+
+public class StaticMethod {
+	static void displ()
+	{
+		System.out.println("welcome");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticMethod.displ();
+
+	}
+
+}
