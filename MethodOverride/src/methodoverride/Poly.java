c4b77f87f361b624800a7027126b6bbb178a7fe6
91818 <91818@LAPTOP-A1HD18TF>
2022-06-25 19:45:30 +0530

    METHOD OVERRIDE program

---
+++ MethodOverride/src/methodoverride/Poly.java
@@ -0,0 +1,30 @@
+package methodoverride;
+class chrome
+{
+  void view()
+  {
+	  System.out.println("parent class");
+  }
+}
+class desktop extends chrome
+{
+	void view()
+	{
+		System.out.println("child class");
+		super.view();
+	}
+}
+public class Poly {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//chrome ob =new chrome();
+		//ob.view();
+		
+		desktop ob1 =new desktop();
+		ob1.view();
+		
+
+	}
+
+}
